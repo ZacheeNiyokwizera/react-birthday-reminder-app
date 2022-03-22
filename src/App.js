307e4a9b9b66bf49ff984576641ca34914a3a37b
Mr Zack <zack@labs.ws>
2022-03-22 18:30:00 +0200

    just testing

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main>
+      <section className='container'>
+
+
+        <h3> 5 birthdays today</h3>
+        {/* display our component here */}
+
+        <button>clear all</button>
+        
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const List = ({ people }) => {
+  return (
+    <>
+    
+              <h4>List of firends here and their birthday</h4>
+              
+      
+    </>
+  );
+};
+
+export default List;
